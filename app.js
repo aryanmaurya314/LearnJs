312beba478214bfb00a67359d0da77a1fc7fec2b
Aryan Maurya <aryanmaurya314@gmail.com>
2022-05-23 13:00:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const res = require('express/lib/response');
+
+
+const app = express();
+
+
+/**
+ * CRUD -> create read update delete
+ * 
+ * GET -> for fetching the data.
+ * POST -> for adding the data.
+ * PUT -> for updating the data.
+ * DELETE -> for deleting the data.
+ */
+
+
+
+
+
+
+
+app.listen(8080, ()=>{
+    console.log('server has started.');
+});
+
+
+
